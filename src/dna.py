66a61a42d14a37d9929c7781748a5bd1a02ad58c
Puneetha Pai B P <puneethp@thoughtworks.com>
2017-04-03 18:40:13 +0530

    Refactor DNA mutation

--- src/dna.py
+++ src/dna.py
@@ -1,10 +1,14 @@
 import random
 
-from nucleotides import Nucleotides
+
+def generate_mutated_structure(structure, indices, mutation_values):
+    for index, nucleotides_value in zip(indices, mutation_values):
+        structure[index] = nucleotides_value
+    structure = ''.join(structure)
+    return structure
 
 
 class DNA(object):
-
     def __init__(self, structure):
         self.structure = structure
 
@@ -22,12 +26,22 @@ class DNA(object):
         return fitness
 
     def mutate(self, percentage, nucleotides):
-        mutated_structure = list(self.structure)
-        mutate_count = len(self.structure) * percentage / 100
-        mutate_indices = random.sample(xrange(len(self.structure)), mutate_count)
-        nucleotides_values = random.sample(nucleotides.nucleotides, mutate_count)
-        for index, nucleotides_value in zip(mutate_indices, nucleotides_values):
-            mutated_structure[index] = nucleotides_value
-        mutated_structure = ''.join(mutated_structure)
+        original_structure = list(self.structure)
+        mutate_indices = self.generate_mutable_indices(percentage)
+        mutation_values = self.generate_random_mutation(nucleotides, percentage)
+        mutated_structure = generate_mutated_structure(original_structure, mutate_indices, mutation_values)
         return DNA(mutated_structure)
 
+    def generate_random_mutation(self, nucleotides, percentage):
+        mutate_count = self.calculate_mutate_count(percentage)
+        nucleotides_values = random.sample(nucleotides.nucleotides, mutate_count)
+        return nucleotides_values
+
+    def generate_mutable_indices(self, percentage):
+        mutate_count = self.calculate_mutate_count(percentage)
+        mutate_indices = random.sample(xrange(len(self.structure)), mutate_count)
+        return mutate_indices
+
+    def calculate_mutate_count(self, percentage):
+        mutate_count = len(self.structure) * percentage / 100
+        return mutate_count
