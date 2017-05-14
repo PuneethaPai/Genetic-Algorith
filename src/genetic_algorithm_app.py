b60f9f0fdf593cbc516c6d2e12f398dbdc7adb16
Puneetha Pai B P <puneethp@thoughtworks.com>
2017-05-14 10:47:48 +0530

    Add class to start app and modify the main accordingly

---
+++ src/genetic_algorithm_app.py
@@ -0,0 +1,35 @@
+import string
+
+
+from dna import DNA
+from generation import Generation
+from nucleotides import Nucleotides
+from population_initialise import PopulationInitialise
+
+
+class StartApp(object):
+    def __init__(self, population_size, kill_percentage, mutate_percentage):
+        self.population_size = population_size
+        self.kill_percentage = kill_percentage
+        self.mutate_percentage = mutate_percentage
+
+    def _initialise(self, target_string):
+        target_length = len(target_string)
+        nucleotides = Nucleotides(string.printable)
+        DNA.nucleotides = nucleotides
+        population_initialise = PopulationInitialise(nucleotides)
+        first_population = population_initialise.create(population_size=self.population_size, structure_size=target_length)
+        first_generation = Generation(1, first_population)
+        return [first_generation]
+
+    def run(self, target_string):
+        self.target = DNA(target_string)
+        self.generations = self._initialise(target_string)
+        result = []
+        while True:
+            next_generation = self.generations[-1].next_generation(self.target)
+            result.append(self.generations[-1].best_fit())
+            if result[-1][0] == self.target:
+                break
+            self.generations.append(next_generation)
+        return result
--- src/main.py
+++ src/main.py
@@ -1,38 +1,15 @@
-import string
 import matplotlib.pyplot as plt
 
-from dna import DNA
-from generation import Generation
-from nucleotides import Nucleotides
-from population import Population
-from population_initialise import PopulationInitialise
+from genetic_algorithm_app import StartApp
 
-target = DNA("I can do anything!!")
-target_length = len(target.structure)
-nucleotides = Nucleotides(string.letters + ') -!;')
-DNA.nucleotides = nucleotides
-Population.kill_percentage = 50
-Population.mutate_percent = 50
-population_initialise = PopulationInitialise(nucleotides)
+app = StartApp(population_size=300, kill_percentage=40, mutate_percentage=20)
+result = app.run(target_string="I can do anything!!")
+for item in result:
+    print item[0].structure, item[1]
 
-first_population = population_initialise.create(population_size=150, structure_size=target_length)
-
-generations = [Generation(number=1, population=first_population)]
-
-iteration = 0
-while True:
-    if generations[-1].population.people[0] == target:
-        break
-    generations.append(generations[-1].next_generation(target))
-    print "Generation : %d Best Fit :%s" % (iteration, generations[-1].population.people[0].structure)
-    iteration += 1
-
-print iteration, generations[-1].population.people[0].structure
-
-x = xrange(iteration)
-y = [generation.population.fitness[0] for generation in generations[:-1]]
-
-plt.plot(x, y, linewidth=2.0)
+x = xrange(len(result))
+y = [item[1] for item in result]
+plt.plot(x, y, linewidth=2.5)
 plt.xlabel('Generation Number')
 plt.ylabel('Fitness Value')
 plt.show()
